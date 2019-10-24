3c4222739ad60995ff071137c35471e38b87d2c5
Gamithra Marga <margamithra@gmail.com>
2019-10-24 19:48:11 +0400

    Added functionality for generating a new tracker and uploading it to a remote server.

---
+++ config.py
@@ -0,0 +1,14 @@
+
+server_name = "https://example.com/" # used to return the tracker's address
+server_web_root = "/var/www/html/" # web root folder of your server
+server_tracker_folder = "hi/" # where the trackers will be stored on the remote server
+tracker_folder = "trackers/" # local folder where the tracker files will be copied
+
+
+ssh_server = "server.com" # ssh server address
+ssh_port = 22
+ssh_user = "user" # must have the correct to permissions to write in the target directory of the server
+ssh_key_location = "/secret/location/key.pem" # location of your ssh key
+
+
+tracker_file = "image.png" # the file that will be uploaded to the server
---
+++ gen.py
@@ -0,0 +1,42 @@
+import subprocess
+import paramiko
+import random
+import string
+from scp import SCPClient
+
+from config import *
+
+
+def createSSHClient(server, port, user, key_filename):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    key = paramiko.RSAKey.from_private_key_file(key_filename)
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, pkey=key)
+    return client
+
+
+def transfer_token(name):
+    ssh = createSSHClient(ssh_server, ssh_port, ssh_user, ssh_key_location)
+    scp = SCPClient(ssh.get_transport())
+    print("SSH and SCP established!")
+
+    scp.put(tracker_folder + name + ".png", server_web_root + server_tracker_folder)
+    print(name + ".png copied to destination folder of remote server")
+
+
+def generate():
+    name = input("Specify token name (leave open to generate random string): ")
+    if name == "":
+        name = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(12))
+
+    subprocess.Popen(("cp " + tracker_file + " " + tracker_folder + name + ".png").split());
+    print("Generated " + name + ".png in the local folder " + tracker_folder)
+
+    transfer_token(name)
+    print("Your token is at:")
+    print(server_name + server_tracker_folder + name + ".png")
+
+
+if __name__ == "__main__":
+    generate()
